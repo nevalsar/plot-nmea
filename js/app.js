4c323bfa68e7b177fffc5ca1da4d651c21353d40
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2015-06-07 02:42:15 +0530

    Parse NMEA data, live update marker
     - Parse NMEA to obtain new latitude and longitude values
     - Update marker location and pan map to new location
    
    Signed-off-by: Nevin Valsaraj <nevin.valsaraj32@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
 var Firebase = require('firebase')
 var nmea = require("nmea")
 
-var firebase_childsnapshot
-var string_nmea
-var string_nmea_parsed
-
 var firebaseRef = new Firebase('https://torid-fire-8608.firebaseio.com');
+var nmea_location
 
 firebaseRef.on('child_changed', function(firebase_childsnapshot, prevChildName) {
     // code to handle new child.
-    console.log("Firebase JSON : ")
+    console.log("Firebase Snapshot: ")
     console.log(firebase_childsnapshot)
 
-    // TODO - parse firebase_childsnapshot to get string_nmea
-    var string_nmea = firebase_childsnapshot
+    var string_nmea = firebase_childsnapshot.val()
     console.log("NMEA Raw : ")
     console.log(string_nmea);
 
@@ -21,11 +17,19 @@ firebaseRef.on('child_changed', function(firebase_childsnapshot, prevChildName) 
         var string_nmea_parsed = nmea.parse(string_nmea)
         console.log("NMEA Parsed : ")
         console.log(string_nmea_parsed)
+
+        var nmea_lat = string_nmea_parsed.lat/100;
+        nmea_lat = (string_nmea_parsed.latPole === "N")? nmea_lat: -nmea_lat;
+        var nmea_lon = string_nmea_parsed.lon/100;
+        nmea_lon = (string_nmea_parsed.lonPole === "E")? nmea_lon: -nmea_lon;
+        console.log("Lat = " + nmea_lat, " Long = ", nmea_lon);
+
+        nmea_location = new google.maps.LatLng(nmea_lat, nmea_lon);
+        marker.setPosition(nmea_location)
+        map.panTo(nmea_location)
     }
     catch(e){
-        console.log("Error : ")
+        console.log("NMEA parse error : ")
         console.log(e);
     }
-
-    // TODO - update marker location and pan to new marker on parsing Lat & Long
 });
